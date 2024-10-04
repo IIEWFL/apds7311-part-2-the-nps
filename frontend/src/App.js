e1ceb88286fa29e274884ed28a231fb9702e7c96
nikkigordhan <ST10108243@vcconnect.edu.za>
2024-10-04 20:34:53 +0200

    Welcome screen design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './components/auth/Welcome';
+import Login from './components/auth/Login'; // Import the Login component
 import './App.css';
 
 function App() {
   return (
-   <Router>
-    
-   </Router>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define the route for the Welcome page */}
+          <Route path="/" element={<Welcome />} />
+          {/* Define the route for the Login page */}
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <p>Please enter your username, account number, and password to log in.</p>
+    </>
+  );
+}
+
+export default Login;
