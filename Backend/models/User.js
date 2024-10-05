1ee07521f54b5be98ffec8db799a01f69bb399a7
nikkigordhan <ST10108243@vcconnect.edu.za>
2024-10-05 17:17:37 +0200

    testing api

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,37 +1,12 @@
 import mongoose from'mongoose';
-//const bcrypt = require('bcryptjs'); // Ensure consistency in the bcrypt library
 
 const UserSchema = new mongoose.Schema({
-    username: { 
-      type: String, 
-      required: true },
-    fullName: { 
-      type: String, 
-      required: true, 
-      unique: true, 
-      trim: true },
-    idNumber: {
-      type: String,
-      required: [true, "ID number is required"],
-      unique: true,
-      trim: true,
-      minlength: [7, "ID number must be at least 6 characters long"],
-      maxlength: [13, "ID number cannot be more than 13 characters long"]
-  },
-    accountNumber: {
-      type: String,
-      required: [true, "Account number is required"],
-      unique: true,
-      trim: true,
-      min: [10000000, "Account number should be at least 8 digits long"],
-      max: [9999999999999999, "Account number should not be more than 16 digits long"]
-  },
-  password: {
-      type: String,
-      required: [true, "Password is required"],
-      minlength: [8, "Password should be at least 8 characters long"]
-  }
+    username: { type: String, required: true, unique: true, trim: true },
+    fullName: { type: String, required: true, unique: true, trim: true },
+    idNumber: { type: String, required: true, unique: true, trim: true },
+    accountNumber: { type: String, required: true, unique: true }, // Ensure accountNumber is unique
+    password: { type: String, required: true },
   });
 
 
-  export default mongoose.model('User', UserSchema); 
+  export default mongoose.model('User', UserSchema);
