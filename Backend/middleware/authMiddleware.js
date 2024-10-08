216fec564deae5bff7dd043300b219d7b04c6e80
SB Nqaba <st10047982@vcconnect.edu.za>
2024-10-08 06:54:47 +0200

    Previous push fixes

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { verify, TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
 
 const { TokenExpiredError, JsonWebTokenError } = jwt;
