1da00e099598003daef45c08de86b3bc835846f3
SB Nqaba <st10047982@vcconnect.edu.za>
2024-10-08 07:09:02 +0200

    Previous push fixes

--- Backend/models/LoginAttempts.js
+++ Backend/models/LoginAttempts.js
@@ -6,9 +6,6 @@ const loginAttemptSchema = new mongoose.Schema({
     successfulLogin: { type: Boolean, required: true, immutable: true,index: true}, // Add index for faster queries
     timeStamp: { type: Date, default: Date.now, immutable: true,expires: '30d'} // Automatically delete documents after 30 days
     
-})
-
-// Create a compound index for common query patterns
-loginAttemptSchema.index({ username: 1, timeStamp: -1 });
+});
 
 export default mongoose.model('LoginAttempt', loginAttemptSchema);
